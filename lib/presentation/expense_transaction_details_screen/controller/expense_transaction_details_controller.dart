530c98713dae9b8787d5e64b82fb6a206e927c62
rakeshreact1 <rakesh.purohit@dhiwise.com>
2022-06-22 15:33:46 +0000

    Code Synced by DhiWise

--- lib/presentation/expense_transaction_details_screen/controller/expense_transaction_details_controller.dart
+++ lib/presentation/expense_transaction_details_screen/controller/expense_transaction_details_controller.dart
@@ -1,18 +1,70 @@
 import '/core/app_export.dart';
 import 'package:flutterexpension/presentation/expense_transaction_details_screen/models/expense_transaction_details_model.dart';
+import 'package:supabase/supabase.dart';
+import 'package:flutterexpension/core/utils/progress_dialog_utils.dart';
+import 'package:flutterexpension/supabase/models/expenses_model.dart';
 
 class ExpenseTransactionDetailsController extends GetxController
     with StateMixin<dynamic> {
   Rx<ExpenseTransactionDetailsModel> expenseTransactionDetailsModelObj =
       ExpenseTransactionDetailsModel().obs;
 
+  var expensesModel = <ExpensesModel>[];
+
   @override
-  void onReady() {
+  void onReady() async {
     super.onReady();
+    fetchExpensesCollection();
   }
 
   @override
   void onClose() {
     super.onClose();
+  }
+
+  Future<void> fetchExpensesCollection() async {
+    try {
+      ProgressDialogUtils.showProgressDialog();
+      var response = await Get.find<SupabaseClient>()
+          .from('expenses')
+          .select()
+          .order('created_at', ascending: false)
+          .limit(1)
+          .execute();
+      ProgressDialogUtils.hideProgressDialog();
+      if (response.status == 200) {
+        onFetchExpensesCollectionSuccess(response.data);
+      } else {
+        onFetchExpensesCollectionError(
+            response.error?.message ?? 'something went wrong !!');
+      }
+    } catch (err) {
+      ProgressDialogUtils.hideProgressDialog();
+      onFetchExpensesCollectionError(err.toString());
+    }
+  }
+
+  void onFetchExpensesCollectionSuccess(dynamic response) {
+    if (response != null) {
+      expensesModel =
+          (response as List).map((e) => ExpensesModel.fromJson(e)).toList();
+      if (expensesModel != null) {
+        expenseTransactionDetailsModelObj.value.claireJovalskiTxt.value =
+            expensesModel.title!.toString();
+        expenseTransactionDetailsModelObj.value.k8500Txt.value =
+            expensesModel.amount!.toString();
+        expenseTransactionDetailsModelObj.value.feb292022Txt.value =
+            expensesModel.date!.toString();
+      }
+    }
+    Get.defaultDialog(
+        onConfirm: () => Get.back(), title: "WOW", middleText: "Fetched");
+  }
+
+  void onFetchExpensesCollectionError(dynamic err) {
+    if (err is String) {
+      Get.rawSnackbar(message: err);
+    }
+    Get.snackbar('', "Not working, Rahul!");
   }
 }
